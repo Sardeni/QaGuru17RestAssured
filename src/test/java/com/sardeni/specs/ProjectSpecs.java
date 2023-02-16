a018ddc037288a8d4590a427a1dd2a5d8b3ebe86
Sardeni <nordisnsk@gmail.com>
2023-02-16 12:06:54 +0400

    save commit

--- src/test/java/com/sardeni/specs/ProjectSpecs.java
+++ src/test/java/com/sardeni/specs/ProjectSpecs.java
@@ -1,28 +1,23 @@
 package com.sardeni.specs;
 
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static com.sardeni.helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.*;
 import static io.restassured.filter.log.LogDetail.*;
+import static io.restassured.http.ContentType.JSON;
 
-    public class ProjectSpecs {
+public class ProjectSpecs {
 
-        public static RequestSpecification RequestSpec = with()
-                .log().all()
-                .filter(withCustomTemplates())
-                .contentType(ContentType.JSON);
-
-
-        public static ResponseSpecification ResponseSpec = new ResponseSpecBuilder()
-                .log(LogDetail.ALL)
-                .build();
-    }
+    public static RequestSpecification RequestSpec = with()
+            .log().all()
+            .filter(withCustomTemplates())
+            .contentType(JSON);
 
 
+    public static ResponseSpecification ResponseSpec = new ResponseSpecBuilder()
+            .log(ALL)
+            .build();
+}
